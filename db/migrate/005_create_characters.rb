dc94bfa5d49466a8ddceb6a28989871135693f09
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-02-27 19:55:01 +0300

    completed

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |s|
+      s.string :name
+      s.integer :actor_id
+      s.integer :show_id
+    end
   end
 end
